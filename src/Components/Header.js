ce77dc28e217e41037ff8c267d251d8e22845de5
DavidStinghen <davidstinghen@gmail.com>
2019-07-22 21:42:02 -0300

    fet: add header component

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <img src="https://i.imgur.com/KDIDiSE.png" />
+      <div>
+        <span>Meu perfil</span>
+        <i className="material-icons">account_circle</i>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
