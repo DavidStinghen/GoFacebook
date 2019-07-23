a12393dfd21894d8ca1a173b98d6ca5525119219
DavidStinghen <davidstinghen@gmail.com>
2019-07-22 21:57:58 -0300

    fet: add app import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import "./App.css";
-import Header from "./Components/Header";
+import Header from "./components/Header";
 import Feed from "./components/Feed";
 
 function App() {
